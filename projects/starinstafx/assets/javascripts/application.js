f2d48b354f07f1ea79fcc62c8d375763f8f3456e
Galih Pranowo <gapraart@gmail.com>
2019-07-26 20:18:45 +0700

    responsive adjustment in small screen

--- projects/starinstafx/assets/javascripts/application.js
+++ projects/starinstafx/assets/javascripts/application.js
@@ -5,10 +5,24 @@ $(function() {
   });
 
   if ($(window).width() < 768) {
-    var rateItemW = 0;
+    /*var rateItemW = 0;
     $('.rate-item').each(function(i) {
       rateItemW += $(this).outerWidth();
       $('.rate-wrap').width(rateItemW+5);
+    });*/
+    $('.rate-wrap').slick({
+      speed: 5000,
+      autoplay: true,
+      autoplaySpeed: 0,
+      centerMode: true,
+      cssEase: 'linear',
+      slidesToShow: 1,
+      slidesToScroll: 1,
+      variableWidth: true,
+      infinite: true,
+      initialSlide: 1,
+      arrows: false,
+      buttons: false
     });
   }
 
